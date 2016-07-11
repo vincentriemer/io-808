dfe0cd4c082d5df7e090686d76db326c3cf71e7e
Vincent Riemer <vincentriemer@gmail.com>
2016-07-11 00:44:33 -0400

    Fix speedcurve deployment

--- speedcurve.sh
+++ speedcurve.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 curl "https://api.speedcurve.com/v1/deploy" \
-    -u your-api-key:$SC_TOKEN \
+    -u $SC_TOKEN:x \
     --request POST \
     --data site_id=7598 \
     --data note=Build%20$TRAVIS_BUILD_NUMBER
