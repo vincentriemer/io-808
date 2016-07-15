8bfc28afc4886628ee4801b0d5dbcc63f2ec58ec
Vincent Riemer <vincentriemer@gmail.com>
2016-07-15 17:41:40 -0400

    Remove speedcurve deployment trigger

--- speedcurve.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-curl "https://api.speedcurve.com/v1/deploy" \
-    -u $SC_TOKEN:x \
-    --request POST \
-    --data site_id=7598 \
-    --data note=Build%20$TRAVIS_BUILD_NUMBER
