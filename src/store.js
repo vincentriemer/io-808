84aecb7771aac74ff160b8b1b738b36d889744f1
Vincent Riemer <vincentriemer@gmail.com>
2016-07-29 19:07:28 -0400

    Add state persistance to localstorage

--- src/store.js
+++ src/store.js
@@ -1,14 +1,41 @@
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
+import Immutable from 'seamless-immutable';
 
-import reducer from 'reducers';
+import persistState, { mergePersistedState, transformState } from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
+import debounce from 'redux-localstorage-debounce';
+import filter from 'redux-localstorage-filter';
+
+import rootReducer from 'reducers';
 import initialState from 'initialState';
 
-const devToolExtension = (() => {
-  if ('production' !== process.env.NODE_ENV) {
-    return window.devToolsExtension ? window.devToolsExtension() : undefined;
-  } else {
-    return undefined;
-  }
-})();
+const middleware = [];
 
-export default createStore(reducer, initialState, devToolExtension);
+if ('production' !== process.env.NODE_ENV && window.devToolsExtension) {
+  middleware.push(window.devToolsExtension());
+}
+
+const reducer = compose(
+  mergePersistedState(
+    (initialState, persistedState) => initialState.merge(persistedState)
+  )
+)(rootReducer);
+
+const storage = compose(
+  transformState(null, Immutable),
+  filter([
+    'instrumentState',
+    'rhythmLengths',
+    'steps',
+    'masterVolume',
+    'tempo',
+    'fineTempo'
+  ]),
+  debounce(100)
+)(adapter(window.localStorage));
+
+middleware.push(persistState(storage, 'io-808'));
+
+const enhancer = compose(...middleware);
+
+export default createStore(reducer, initialState, enhancer);
