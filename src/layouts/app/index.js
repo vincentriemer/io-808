df46a6b79565511b35eed345df9ee01e6a82dc82
Vincent Riemer <vincentriemer@gmail.com>
2016-08-05 11:37:51 -0400

    Add Github repo link

--- src/layouts/app/index.js
+++ src/layouts/app/index.js
@@ -4,7 +4,7 @@ import Radium from 'radium';
 import { GatewayProvider, GatewayDest } from 'react-gateway';
 
 // Theme
-import { grey } from 'theme/variables';
+import { grey, slightlyDarkerBlack } from 'theme/variables';
 
 // Sub-layouts
 import TopLeftSection from 'layouts/topLeftSection';
@@ -12,7 +12,21 @@ import TopRightSection from 'layouts/topRightSection';
 import BottomSection from 'layouts/bottomSection';
 
 // Components
+import Octicon from 'react-octicon';
 import { ConnectedSaveButton, ConnectedLoadButton } from './connectedComponents';
+
+const GithubLink = Radium(() => (
+  <a style={{
+    color: slightlyDarkerBlack,
+    opacity: 0.5,
+    transition: 'opacity 0.2s',
+    ':hover': {
+      opacity: 1.0
+    }
+  }} href="https://github.com/vincentriemer/io-808" target="_blank" title="Github Repo">
+    <Octicon name="mark-github" mega/>
+  </a>
+));
 
 // layout constants
 const APP_WIDTH = 1400;
@@ -116,6 +130,7 @@ class AppLayout extends React.Component {
                 <ConnectedLoadButton size={35} />
                 <ConnectedSaveButton size={35} />
               </div>
+              <GithubLink />
             </div>
             <div style={styles.appWrapper}>
               <div style={styles.topBottomDivider}></div>
