63e81786e26c2150604a92d7b7b95362db9c76ef
Jonathan Reed <jontonsoup4@gmail.com>
2016-11-24 11:42:21 -0500

    Add button shadows, switch transitions, and fixed cursor on button text

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -22,19 +22,21 @@ class Button extends React.Component {
 
   render() {
     const {style, children, disabled=false, onClick=()=>{}} = this.props;
-
     const styles = {
       button: {
         backgroundColor: grey,
         width: 80, height: 40,
-        transition: 'transform 0.05s, opacity 0.5s',
+        transition: 'transform cubic-bezier(0.4, 0.0, 0.2, 1) .1s, opacity 0.5s',
         transform: 'scale(1.0) translateZ(0)',
+        boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
         ':hover': {
           cursor: 'pointer',
-          transform: style.transform ? style.transform : '' + 'scale(1.04) translateZ(0)'
+          transform: style && style.transform ? style.transform : 'scale(1.04) translateZ(0)',
+          boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
         },
         ':active': {
-          transform: style.transform ? style.transform : '' + 'scale(1.0) translateZ(0)'
+          transform: style && style.transform ? style.transform : 'scale(1.0) translateZ(0)',
+          boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)"
         },
         userSelect: 'none',
 
