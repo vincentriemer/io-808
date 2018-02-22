f61ffb085f051a5c0484d30f6f9bd91023e1b413
Wowa Barsukov <pro.barsukov@gamil.com>
2018-02-22 20:37:00 +0100

    Modify server to support link and socket

--- devServer.js
+++ devServer.js
@@ -3,8 +3,28 @@ var webpack = require('webpack');
 var config = require('./webpack.config.dev');
 var historyApiFallback = require('connect-history-api-fallback');
 
-var app = express();
 var compiler = webpack(config);
+var app = express();
+var server = app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3000');
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(client){
+  client.on('event', function(data){});
+  client.on('disconnect', function(){});
+});
+
+const abletonlink = require('abletonlink');
+const link = new abletonlink();
+link.startUpdate(60, (beat, phase, bpm) => {
+  io.emit('action', {type: 'tempo', payload: {bpm: bpm} });
+});
 
 app.use(historyApiFallback({
   verbose: false
@@ -18,11 +38,3 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
 app.use(devMiddleware);
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.listen(3000, function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  console.log('Listening at http://localhost:3000');
-});
