c550c3c1fd29337f668a6cd728d369546c7f7e45
Wowa Barsukov <pro.barsukov@gamil.com>
2018-02-23 23:49:04 +0100

    Add tempo label

--- src/layouts/app/connectedComponents.js
+++ src/layouts/app/connectedComponents.js
@@ -1,36 +1,44 @@
-import { connect } from 'react-redux';
-import React from 'react';
+import { connect } from "react-redux";
+import React from "react";
 
 // Action Creators
-import { onStateLoad, onReset, onLinkChange } from 'actionCreators';
+import { onStateLoad, onReset, onLinkChange } from "actionCreators";
 
 // Components
-import SaveButton from 'components/saveButton';
-import LoadButton from 'components/loadButton';
-import Button from 'components/button';
+import SaveButton from "components/saveButton";
+import LoadButton from "components/loadButton";
+import Button from "components/button";
 
-import { buttonColor, darkGrey } from 'theme/variables';
-import { labelGreyLarge } from 'theme/mixins'
+import { buttonColor, darkGrey } from "theme/variables";
+import { labelGreyLarge } from "theme/mixins";
+import InstrumentLabel from "components/instrumentLabel";
 
-const createButton = (name) => {
-  return ({ size, ...rest}) => (
-     <Button {...rest} style={{
-       ...labelGreyLarge,
-       color: darkGrey,
-       width: 'auto', height: size,
-       padding: 7,
-       borderRadius: 4,
-       backgroundColor: buttonColor,
-       marginLeft: 5, marginRight: 5,
-   
-       display: 'flex',
-       alignItems: 'center'
-     }}>{name}</Button>
-   );
- }
- 
+const createButton = name => {
+  return ({ size, ...rest }) => (
+    <Button
+      {...rest}
+      style={{
+        ...labelGreyLarge,
+        color: darkGrey,
+        width: "auto",
+        height: size,
+        padding: 7,
+        borderRadius: 4,
+        backgroundColor: buttonColor,
+        marginLeft: 5,
+        marginRight: 5,
+
+        display: "flex",
+        alignItems: "center"
+      }}
+    >
+      {name}
+    </Button>
+  );
+};
+
 export const ConnectedSaveButton = (() => {
-  const mapStateToProps = (state) => ({
+  const mapStateToProps = state => ({
     storeState: state
   });
 
@@ -38,39 +46,55 @@ export const ConnectedSaveButton = (() => {
 })();
 
 export const ConnectedLoadButton = (() => {
-  const mapStateToProps = (state) => ({
+  const mapStateToProps = state => ({
     playing: state.playing
   });
 
-  const mapDispatchToProps = (dispatch) => ({
-    onLoadedState: (loadedState) => dispatch(onStateLoad(loadedState))
+  const mapDispatchToProps = dispatch => ({
+    onLoadedState: loadedState => dispatch(onStateLoad(loadedState))
   });
 
   return connect(mapStateToProps, mapDispatchToProps)(LoadButton);
 })();
 
 export const ConnectedAbletonLinkButton = (() => {
-  const mapStateToProps = (state) => ({
+  const mapStateToProps = state => ({
     tempo: state.tempo
   });
 
-  const mapDispatchToProps = (dispatch) => ({
+  const mapDispatchToProps = dispatch => ({
     onClick: () => {
       dispatch(onLinkChange());
     }
   });
-  return connect(mapStateToProps, mapDispatchToProps)(createButton("Ablteon Link"));
+  return connect(mapStateToProps, mapDispatchToProps)(
+    createButton("Ablteon Link")
+  );
 })();
 
+export const ConnectedAbletonLabel = (() => {
+  const mapStateToProps = state => ({
+    label: [Math.round(state.tempo).toString()],
+    isLinkEnabled: state.isLinkEnabled
+  });
+
+  return connect(mapStateToProps)(({ isLinkEnabled, ...rest }) => {
+    if (isLinkEnabled) {
+      return <InstrumentLabel {...rest} />;
+    } else {
+      return null;
+    }
+  });
+})();
 
 export const ConnectedResetButton = (() => {
-  const mapStateToProps = (state) => ({
+  const mapStateToProps = state => ({
     disabled: state.playing
   });
 
-  const mapDispatchToProps = (dispatch) => ({
+  const mapDispatchToProps = dispatch => ({
     onClick: () => {
-      if (confirm('Are you sure you want to reset your sequencer?'))
+      if (confirm("Are you sure you want to reset your sequencer?"))
         dispatch(onReset());
     }
   });
--- src/layouts/app/index.js
+++ src/layouts/app/index.js
@@ -1,34 +1,44 @@
 // External Deps
-import React from 'react';
-import Radium from 'radium';
-import { GatewayProvider, GatewayDest } from 'react-gateway';
+import React from "react";
+import Radium from "radium";
+import { GatewayProvider, GatewayDest } from "react-gateway";
 
 // Theme
-import { grey, slightlyDarkerBlack } from 'theme/variables';
-import { labelGreyLarge, autoCursor } from 'theme/mixins';
+import { grey, slightlyDarkerBlack } from "theme/variables";
+import { labelGreyLarge, autoCursor } from "theme/mixins";
 
 // Sub-layouts
-import TopLeftSection from 'layouts/topLeftSection';
-import TopRightSection from 'layouts/topRightSection';
-import BottomSection from 'layouts/bottomSection';
+import TopLeftSection from "layouts/topLeftSection";
+import TopRightSection from "layouts/topRightSection";
+import BottomSection from "layouts/bottomSection";
 
 // Components
-import Octicon from 'react-octicon';
-import { ConnectedSaveButton, ConnectedLoadButton, ConnectedResetButton, ConnectedAbletonLinkButton } from './connectedComponents';
+import Octicon from "react-octicon";
+import {
+  ConnectedSaveButton,
+  ConnectedLoadButton,
+  ConnectedResetButton,
+  ConnectedAbletonLinkButton,
+  ConnectedAbletonLabel
+} from "./connectedComponents";
 
 const GithubLink = Radium(() => (
-  <a style={{
-    color: slightlyDarkerBlack,
-    opacity: 0.5,
-    transition: 'opacity 0.2s',
-    ':hover': {
-      opacity: 1.0
-    }
-  }} href="https://github.com/vincentriemer/io-808" target="_blank" title="Github Repo">
-    <Octicon name="mark-github" mega/>
+  <a
+    style={{
+      color: slightlyDarkerBlack,
+      opacity: 0.5,
+      transition: "opacity 0.2s",
+      ":hover": {
+        opacity: 1.0
+      }
+    }}
+    href="https://github.com/vincentriemer/io-808"
+    target="_blank"
+    title="Github Repo"
+  >
+    <Octicon name="mark-github" mega />
   </a>
 ));
-
 
 // layout constants
 const APP_WIDTH = 1400;
@@ -39,13 +49,13 @@ const HEADER_HEIGHT = 50;
 const FOOTER_HEIGHT = 30;
 
 const TOP_BOTTOM_DIVIDER_HEIGHT = 3;
-const TOP_HEIGHT = Math.ceil(APP_HEIGHT * 0.64) - (TOP_BOTTOM_DIVIDER_HEIGHT * 2);
-const BOTTOM_HEIGHT = APP_HEIGHT - TOP_HEIGHT - (TOP_BOTTOM_DIVIDER_HEIGHT * 2);
+const TOP_HEIGHT = Math.ceil(APP_HEIGHT * 0.64) - TOP_BOTTOM_DIVIDER_HEIGHT * 2;
+const BOTTOM_HEIGHT = APP_HEIGHT - TOP_HEIGHT - TOP_BOTTOM_DIVIDER_HEIGHT * 2;
 
 const INSTRUMENT_SEPERATOR_WIDTH = 1;
 
 const TOP_LEFT_WIDTH = Math.ceil(APP_WIDTH * 0.23) - INSTRUMENT_SEPERATOR_WIDTH;
-const TOP_RIGHT_WIDTH = APP_WIDTH - TOP_LEFT_WIDTH ;
+const TOP_RIGHT_WIDTH = APP_WIDTH - TOP_LEFT_WIDTH;
 
 const TOP_HORIZONTAL_SEPERATOR_HEIGHT = TOP_HEIGHT - 10;
 
@@ -58,68 +68,82 @@ class AppLayout extends React.Component {
   render() {
     const styles = {
       pageWrapper: {
-        position: 'relative',
-        width: '100%', height: '100%',
-        minWidth: APP_WIDTH + APP_PADDING, minHeight: APP_HEIGHT + HEADER_HEIGHT + FOOTER_HEIGHT + APP_PADDING
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        minWidth: APP_WIDTH + APP_PADDING,
+        minHeight: APP_HEIGHT + HEADER_HEIGHT + FOOTER_HEIGHT + APP_PADDING
       },
 
       wrapper: {
-        position: 'absolute',
-        width: APP_WIDTH, height: APP_HEIGHT + HEADER_HEIGHT + FOOTER_HEIGHT,
-        top: 0, left: 0, right: 0, bottom: 0,
-        margin: 'auto',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
+        position: "absolute",
+        width: APP_WIDTH,
+        height: APP_HEIGHT + HEADER_HEIGHT + FOOTER_HEIGHT,
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        margin: "auto",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
       },
 
       headerWrapper: {
-        width: APP_WIDTH, height: HEADER_HEIGHT,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between'
+        width: APP_WIDTH,
+        height: HEADER_HEIGHT,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
       },
 
       saveLoadClearWrapper: {
-        display: 'flex',
-        flexDirection: 'row'
+        display: "flex",
+        flexDirection: "row"
       },
 
       footerWrapper: {
-        width: APP_WIDTH, height: FOOTER_HEIGHT,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingLeft: 10, paddingRight: 10
+        width: APP_WIDTH,
+        height: FOOTER_HEIGHT,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingLeft: 10,
+        paddingRight: 10
       },
 
       appWrapper: {
-        width: APP_WIDTH, height: APP_HEIGHT,
-        display: 'flex',
-        flexDirection: 'column'
+        width: APP_WIDTH,
+        height: APP_HEIGHT,
+        display: "flex",
+        flexDirection: "column"
       },
 
       topBottomDivider: {
-        width: APP_WIDTH, height: TOP_BOTTOM_DIVIDER_HEIGHT,
+        width: APP_WIDTH,
+        height: TOP_BOTTOM_DIVIDER_HEIGHT,
         backgroundColor: grey
       },
 
       topHorizontalDivider: {
-        width: INSTRUMENT_SEPERATOR_WIDTH, height: TOP_HORIZONTAL_SEPERATOR_HEIGHT,
+        width: INSTRUMENT_SEPERATOR_WIDTH,
+        height: TOP_HORIZONTAL_SEPERATOR_HEIGHT,
         backgroundColor: grey
       },
 
       topWrapper: {
-        width: APP_WIDTH, height: TOP_HEIGHT,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center'
+        width: APP_WIDTH,
+        height: TOP_HEIGHT,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center"
       },
 
       bottomWrapper: {
-        width: APP_WIDTH, height: BOTTOM_HEIGHT
+        width: APP_WIDTH,
+        height: BOTTOM_HEIGHT
       },
 
       footerText: {
@@ -139,34 +163,55 @@ class AppLayout extends React.Component {
                 <ConnectedSaveButton size={35} />
                 <ConnectedResetButton size={35} />
                 <ConnectedAbletonLinkButton size={35} />
+                <ConnectedAbletonLabel />
               </div>
               <GithubLink />
             </div>
             <div style={styles.appWrapper}>
-              <div style={styles.topBottomDivider}></div>
+              <div style={styles.topBottomDivider} />
               <div style={styles.topWrapper}>
                 <TopLeftSection width={TOP_LEFT_WIDTH} height={TOP_HEIGHT} />
-                <div style={styles.topHorizontalDivider}></div>
-                <TopRightSection width={TOP_RIGHT_WIDTH} height={TOP_HEIGHT}
-                                 seperatorWidth={INSTRUMENT_SEPERATOR_WIDTH} />
-                <div style={styles.topHorizontalDivider}></div>
+                <div style={styles.topHorizontalDivider} />
+                <TopRightSection
+                  width={TOP_RIGHT_WIDTH}
+                  height={TOP_HEIGHT}
+                  seperatorWidth={INSTRUMENT_SEPERATOR_WIDTH}
+                />
+                <div style={styles.topHorizontalDivider} />
               </div>
-              <div style={styles.topBottomDivider}></div>
+              <div style={styles.topBottomDivider} />
               <div style={styles.bottomWrapper}>
-                <BottomSection width={APP_WIDTH} height={BOTTOM_HEIGHT} topLeftWidth={TOP_LEFT_WIDTH} />
+                <BottomSection
+                  width={APP_WIDTH}
+                  height={BOTTOM_HEIGHT}
+                  topLeftWidth={TOP_LEFT_WIDTH}
+                />
               </div>
             </div>
             <div style={styles.footerWrapper}>
               <div>
-                <a style={styles.footerText} href="/tutorial" target="_blank">Tutorial</a>
+                <a style={styles.footerText} href="/tutorial" target="_blank">
+                  Tutorial
+                </a>
               </div>
               <div style={labelGreyLarge}>
-                Made with <Octicon name="heart" /> by <a style={{ color: grey }} href="http://vincentriemer.com" target="_blank">
+                Made with <Octicon name="heart" /> by{" "}
+                <a
+                  style={{ color: grey }}
+                  href="http://vincentriemer.com"
+                  target="_blank"
+                >
                   Vincent Riemer
                 </a>
               </div>
               <div>
-                <a style={styles.footerText} href="https://github.com/vincentriemer/io-808/issues" target="_blank">Report an Issue</a>
+                <a
+                  style={styles.footerText}
+                  href="https://github.com/vincentriemer/io-808/issues"
+                  target="_blank"
+                >
+                  Report an Issue
+                </a>
               </div>
             </div>
           </div>
