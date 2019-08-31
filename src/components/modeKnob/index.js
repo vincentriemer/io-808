c268097bbe5d2d52a9fc437eb129b7968e779f19
Vincent Riemer <vincentriemer@gmail.com>
2019-08-31 11:26:34 -0700

    migrate modeKnob to hooks

--- src/components/modeKnob/index.js
+++ src/components/modeKnob/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import Knob from "components/knob";
 import Guides from "components/guides";
@@ -8,68 +7,53 @@ import SelectorKnobInner from "components/selectorKnobInner";
 import { grey } from "theme/variables";
 import { ring } from "theme/mixins";
 
-class ModeKnob extends React.Component {
-  render() {
-    const { value, onChange, size = 100 } = this.props;
+const styles = {
+  wrapper: {
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "space-between"
+  },
+  controlWrapper: {
+    position: "relative"
+  },
+  knobWrapper: ring("100%"),
+  guides: {
+    width: 5,
+    height: 5,
+    backgroundColor: grey,
+    borderRadius: "50%"
+  }
+};
 
-    const styles = {
-      wrapper: {
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "stretch",
-        justifyContent: "space-between",
-        width: size,
-        height: size
-      },
-
-      controlWrapper: {
-        position: "relative",
-        width: size,
-        height: size
-      },
-
-      knobWrapper: ring("100%"),
-
-      guides: {
-        width: 5,
-        height: 5,
-        backgroundColor: grey,
-        borderRadius: "50%"
-      }
-    };
-
-    return (
-      <div style={styles.wrapper}>
-        <div style={styles.controlWrapper}>
-          <Guides
-            num={6}
-            distance={size * 0.58}
-            hideCount={6}
-            guideStyle={styles.guides}
-          />
-          <div style={styles.knobWrapper}>
-            <Knob
-              value={value}
-              onChange={onChange}
-              size={size}
-              bufferSize={150}
-              min={0}
-              max={5}
-              step={1}
-            >
-              <SelectorKnobInner size={size} />
-            </Knob>
-          </div>
+const ModeKnob = props => {
+  const { value, onChange, size = 100 } = props;
+  return (
+    <div style={{ ...styles.wrapper, width: size, height: size }}>
+      <div style={{ ...styles.controlWrapper, width: size, height: size }}>
+        <Guides
+          num={6}
+          distance={size * 0.58}
+          hideCount={6}
+          guideStyle={styles.guides}
+        />
+        <div style={styles.knobWrapper}>
+          <Knob
+            value={value}
+            onChange={onChange}
+            size={size}
+            bufferSize={150}
+            min={0}
+            max={5}
+            step={1}
+          >
+            <SelectorKnobInner size={size} />
+          </Knob>
         </div>
       </div>
-    );
-  }
-}
-
-ModeKnob.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.number.isRequired
+    </div>
+  );
 };
 
 export default ModeKnob;
