d10cbc09f8d697613395160e49c77655e1fe1b88
Vincent Riemer <vincentriemer@gmail.com>
2019-08-31 08:27:43 -0700

    migrate arrowLabel to hooks

--- src/components/arrowLabel/index.js
+++ src/components/arrowLabel/index.js
@@ -1,93 +1,87 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import Radium from "radium";
 
 import { labelGreyNormal } from "theme/mixins";
 
-class ArrowLabel extends React.Component {
-  static propTypes = {
-    label: PropTypes.string.isRequired,
-    width: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired,
-    textColor: PropTypes.string.isRequired,
-    backgroundColor: PropTypes.string.isRequired,
-    direction: PropTypes.oneOf(["right", "left"])
-  };
+const styles = {
+  wrapper: {
+    position: "relative",
+    display: "flex",
+    alignItems: "center"
+  },
 
-  render() {
-    const {
-      label,
-      width,
-      height,
-      textColor,
-      backgroundColor,
-      direction
-    } = this.props;
+  arrowPoint: {
+    width: 0,
+    height: 0
+  },
 
-    const arrowEndWidth = height;
-    const arrowShaftWidth = height / 4;
-    const arrowShaftHeight = height / 3;
-    const labelWrapperWidth = width - arrowEndWidth - arrowShaftWidth;
+  arrowShaft: {
+    position: "relative",
+    transform: "scaleX(1.1)"
+  },
 
-    const transparentTriangeSides = `${(arrowEndWidth * 3) /
-      8}px solid transparent`;
-    const coloredTriangleSide = `${arrowEndWidth /
-      2}px solid ${backgroundColor}`;
+  labelWrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 1
+  },
 
-    const styles = {
-      wrapper: {
-        position: "relative",
+  label: {
+    ...labelGreyNormal
+  }
+};
+
+const ArrowLabel = props => {
+  const { label, width, height, textColor, backgroundColor, direction } = props;
+
+  const arrowEndWidth = height;
+  const arrowShaftWidth = height / 4;
+  const arrowShaftHeight = height / 3;
+  const labelWrapperWidth = width - arrowEndWidth - arrowShaftWidth;
+
+  const transparentTriangeSides = `${(arrowEndWidth * 3) /
+    8}px solid transparent`;
+  const coloredTriangleSide = `${arrowEndWidth / 2}px solid ${backgroundColor}`;
+
+  return (
+    <div
+      style={{
+        ...styles.wrapper,
         width,
         height,
-        display: "flex",
-        alignItems: "center",
         flexDirection: direction === "left" ? "row-reverse" : "row"
-      },
-
-      arrowPoint: {
-        width: 0,
-        height: 0,
-        borderTop: transparentTriangeSides,
-        borderBottom: transparentTriangeSides,
-        [direction === "left"
-          ? "borderRight"
-          : "borderLeft"]: coloredTriangleSide
-      },
-
-      arrowShaft: {
-        position: "relative",
-        width: arrowShaftWidth,
-        height: arrowShaftHeight,
-        transform: "scaleX(1.1)",
-        backgroundColor
-      },
-
-      labelWrapper: {
-        width: labelWrapperWidth + arrowEndWidth / 2,
-        height,
-        backgroundColor,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 1
-      },
-
-      label: {
-        ...labelGreyNormal,
-        color: textColor
-      }
-    };
-
-    return (
-      <div style={styles.wrapper}>
-        <div style={styles.labelWrapper}>
-          <div style={styles.label}>{label}</div>
-        </div>
-        <div style={styles.arrowShaft} />
-        <div style={styles.arrowPoint} />
+      }}
+    >
+      <div
+        style={{
+          ...styles.labelWrapper,
+          width: labelWrapperWidth + arrowEndWidth / 2,
+          height,
+          backgroundColor
+        }}
+      >
+        <div style={{ ...styles.label, color: textColor }}>{label}</div>
       </div>
-    );
-  }
-}
+      <div
+        style={{
+          ...styles.arrowShaft,
+          width: arrowShaftWidth,
+          height: arrowShaftHeight,
+          backgroundColor
+        }}
+      />
+      <div
+        style={{
+          ...styles.arrowPoint,
+          borderTop: transparentTriangeSides,
+          borderBottom: transparentTriangeSides,
+          [direction === "left"
+            ? "borderRight"
+            : "borderLeft"]: coloredTriangleSide
+        }}
+      />
+    </div>
+  );
+};
 
-export default Radium(ArrowLabel);
+export default ArrowLabel;
