bcfc64e158aafba32701ee32bc9f5062f6b5e3fb
Vincent Riemer <vincentriemer@gmail.com>
2019-08-31 11:11:37 -0700

    migrate light to hooks

--- src/components/light/index.js
+++ src/components/light/index.js
@@ -1,56 +1,47 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 import { lightActive, lightInactive } from "theme/variables";
 
 const size = 18;
 const innerPadding = 4;
 
-class Light extends React.Component {
-  render() {
-    const { active } = this.props;
+const baseInnerStyle = {
+  position: "absolute",
+  left: innerPadding,
+  right: innerPadding,
+  top: innerPadding,
+  bottom: innerPadding,
+  borderRadius: "50%"
+};
 
-    const baseInnerStyle = {
-      position: "absolute",
-      left: innerPadding,
-      right: innerPadding,
-      top: innerPadding,
-      bottom: innerPadding,
-      borderRadius: "50%"
-    };
-
-    const styles = {
-      outer: {
-        position: "relative",
-        backgroundColor: "rgba(0,0,0,0.4)",
-        width: size,
-        height: size,
-        borderRadius: "50%",
-        pointerEvents: "none"
-      },
-      innerInactive: {
-        ...baseInnerStyle,
-        backgroundColor: lightInactive
-      },
-      innerActive: {
-        ...baseInnerStyle,
-        backgroundColor: lightActive,
-        transition: "opacity 0.1s",
-        opacity: active ? 1 : 0
-      }
-    };
-
-    return (
-      <div style={styles.outer}>
-        <div style={styles.innerInactive} />
-        <div style={styles.innerActive} />
-      </div>
-    );
+const styles = {
+  outer: {
+    position: "relative",
+    backgroundColor: "rgba(0,0,0,0.4)",
+    width: size,
+    height: size,
+    borderRadius: "50%",
+    pointerEvents: "none"
+  },
+  innerInactive: {
+    ...baseInnerStyle,
+    backgroundColor: lightInactive
+  },
+  innerActive: {
+    ...baseInnerStyle,
+    backgroundColor: lightActive,
+    transition: "opacity 0.1s"
   }
-}
+};
 
-Light.propTypes = {
-  active: PropTypes.bool.isRequired
+const Light = props => {
+  const { active } = props;
+  return (
+    <div style={styles.outer}>
+      <div style={styles.innerInactive} />
+      <div style={{ ...styles.innerActive, opacity: active ? 1 : 0 }} />
+    </div>
+  );
 };
 
 export default Light;
